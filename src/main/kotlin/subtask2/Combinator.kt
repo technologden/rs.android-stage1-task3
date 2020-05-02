130d3f1055a153443a1285f74d595ac30a7ba64f
Denis Drozdov <drazdou.work@gmail.com>
2020-05-02 17:55:00 +0300

    feat: add solution subtask2 Combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,31 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val numberOfPosters = array[0]
+        val numberOfColors = array[1]
+
+        if (numberOfPosters < 1 || numberOfColors < 1)
+            return null
+
+        var colorCount = 0
+
+        for (i in 1 until numberOfColors){
+            if (getCombination(numberOfColors, i) == numberOfPosters
+                && (colorCount > i || colorCount == 0))
+                colorCount = i
+        }
+
+        if (colorCount<1)
+            return null
+        return colorCount
+    }
+
+    //Binomial Coefficient
+    private fun getCombination(n: Int, k: Int): Int{
+        if (k == 1)
+            return n
+        if (n == k)
+            return 1
+        return getCombination(n-1, k) + getCombination(n - 1, k - 1)
     }
 }
