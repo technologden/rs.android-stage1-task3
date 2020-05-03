84979edb432b9d9371a6fa6bafd40ab830a16df5
Denis Drozdov <drazdou.work@gmail.com>
2020-05-03 20:12:30 +0300

    feat: add solution subtask4 SquareDecomposer

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,42 @@
 package subtask4
 
+import temp.decomposer
+import java.util.ArrayList
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val numberL = number.toLong()
+        val decomposeList = decomposer(numberL, numberL * numberL)
+        if (decomposeList.isNullOrEmpty()) return null
+
+        decomposeList.removeAt(decomposeList.size - 1)
+
+        val result = mutableListOf<Int>()
+        for (i in decomposeList)
+            result.add(i.toInt())
+        return result.toTypedArray()
+    }
+
+    private fun decomposer(n: Long, remain: Long): MutableList<Long>? { // basic case
+        var result:  MutableList<Long>? = mutableListOf()
+        if (remain == 0L) {
+            result?.add(n)
+            return result
+        }
+
+        for (i in n - 1 downTo 1) {
+            if (remain - i * i >= 0) {
+                result = decomposer(i, remain - i * i)
+
+                if (result != null) {
+                    result.add(n)
+                    return result
+                }
+            }
+        }
+
+        return null
     }
 }
