0adbd6faa2365c3bd2a3cb1d8863151e79152bb4
Denis Drozdov <drazdou.work@gmail.com>
2020-05-05 12:28:43 +0300

    feat: add ST3

--- src/main/kotlin/temp/ST3.kt
+++ src/main/kotlin/temp/ST3.kt
@@ -3,43 +3,70 @@ package temp
 import kotlin.math.abs
 
 fun main() {
-    val number = 3
-    val array1 = arrayOf(-100, -2, 50, -7, 1, 3, 10)
-    val array2 = arrayOf("1", 2.5, 1)
-    val array3 = arrayOf(" ", "q")
+    var num = 4
+    val array1 = arrayOf(-100, -2, 50, -7, 1, 3, 10, "r")
 
-    println(maxProductOf(number, arrayOf(-1000000, 20, 5, 1, 7, -5)))
+    val list = array1.filterIsInstance<Int>().sorted()
+    var start = 0
+    var end = list.size - 1
+    var res = 1
 
+    while (num>=2){
+        val startArr = list[start]*list[start + 1]
+        val endArr = list[end]*list[end - 1]
 
+        println("Start $startArr")
+        println("End $endArr")
 
-}
+        if (startArr > endArr){
+            res *= startArr
+            start+=2
+        }else{
+            res *= endArr
+            end -= 2
+        }
 
-fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-    val sortList = itemsFromArray
-        .filterIsInstance<Int>()
-        .sortedBy { abs(it) }.reversed()
+        println("Start $start")
+        println("End $end")
 
-    if (sortList.isEmpty()){return 0}
-
-
-    var result = 1
-
-    if (sortList.size<= numberOfItems){
-        for (i in sortList)
-            result *=i
-        return result
+        num -= 2
     }
-    for (i in 0 until numberOfItems)
-        result *= sortList[i]
+    if (num == 1)
+        res *= maxOf(list[start], list[end])
 
-    val positiveList = sortList.filter { i -> i > 0  }
-    var resultPositive = 1
 
-    for (i in 0 until numberOfItems)
-        resultPositive *= positiveList[i]
 
-    if (resultPositive>=result)
-        return resultPositive
-    else
-        return result
+    println(res)
 }
+
+
+//fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
+//    val sortList = itemsFromArray
+//        .filterIsInstance<Int>()
+//        .sortedBy { abs(it) }.reversed()
+//
+//    if (sortList.isEmpty()){return 0}
+//
+//
+//    var result = 1
+//
+//    if (sortList.size<= numberOfItems){
+//        for (i in sortList)
+//            result *=i
+//        return result
+//    }
+//    for (i in 0 until numberOfItems)
+//        result *= sortList[i]
+//
+//    val positiveList = sortList.filter { i -> i > 0  }
+//    var resultPositive = 1
+//
+//    for (i in 0 until numberOfItems)
+//        resultPositive *= positiveList[i]
+//
+//    if (resultPositive>=result)
+//        return resultPositive
+//    else
+//        return result
+//}
+
